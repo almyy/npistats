0d948c8e2b96fdf6bf5dd9217c17b54d6bef9b55
Mikael Solstad <mikaelrinosolstad@gmail.com>
2018-01-13 17:04:20 +0100

    Added building of scss. Connected with graphql backend#

--- src/components/ownerlist/OwnerList.js
+++ src/components/ownerlist/OwnerList.js
@@ -6,22 +6,36 @@ import { graphql } from 'react-apollo';
 
 import { ALL_OWNERS_QUERY } from '../../graphql/queries';
 
+import style from './OwnerList.css';
+
 type OwnerListProps = {
-    owners: Array<OwnerType>
+    data: {
+      allOwners: Array<OwnerType>,
+      loading: boolean,
+    }
 }
 
-const OwnerListItem = (props) => {
+const OwnerListItem = ({owner}: {owner: OwnerType}) => {
     return (
-        <div> </div>
+        <div className={style.root}> 
+          {owner.ownerName} 
+          
+        </div>
     )
 }
 
 class OwnerList extends Component<OwnerListProps> {
   render() {
+    console.log(this.props)
+    const { data: {loading, allOwners  }} = this.props;
+    if (loading) return <div/>
     return (
       <div className="App">
-        
-
+        {
+          allOwners.map(owner => {
+            return <OwnerListItem owner={owner} />
+          })
+        }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// 2
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
-// 3
+const httpLink = new HttpLink({ uri: '/graphql'})
+
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
